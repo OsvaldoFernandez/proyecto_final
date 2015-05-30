a18852a9f260e657f9be599794483b563a2f1dde
Osvaldo Fernandez <osvaldo.g.fernandez@gmail.com>
2015-05-30 19:41:00 -0300

    Primer commit de la solución

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace clienteMail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataMails = new System.Windows.Forms.DataGridView();
+            this.Nro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.De = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Asunto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnRecibidos = new System.Windows.Forms.Button();
+            this.btnEnviados = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataMails)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataMails
+            // 
+            this.dataMails.AllowUserToAddRows = false;
+            this.dataMails.AllowUserToDeleteRows = false;
+            this.dataMails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataMails.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nro,
+            this.De,
+            this.Asunto,
+            this.Fecha});
+            this.dataMails.Location = new System.Drawing.Point(14, 58);
+            this.dataMails.Name = "dataMails";
+            this.dataMails.ReadOnly = true;
+            this.dataMails.Size = new System.Drawing.Size(622, 225);
+            this.dataMails.TabIndex = 1;
+            // 
+            // Nro
+            // 
+            this.Nro.Frozen = true;
+            this.Nro.HeaderText = "Nº";
+            this.Nro.Name = "Nro";
+            this.Nro.ReadOnly = true;
+            this.Nro.Width = 30;
+            // 
+            // De
+            // 
+            this.De.Frozen = true;
+            this.De.HeaderText = "De";
+            this.De.Name = "De";
+            this.De.ReadOnly = true;
+            this.De.Width = 120;
+            // 
+            // Asunto
+            // 
+            this.Asunto.Frozen = true;
+            this.Asunto.HeaderText = "Asunto";
+            this.Asunto.Name = "Asunto";
+            this.Asunto.ReadOnly = true;
+            this.Asunto.Width = 300;
+            // 
+            // Fecha
+            // 
+            this.Fecha.Frozen = true;
+            this.Fecha.HeaderText = "Fecha";
+            this.Fecha.Name = "Fecha";
+            this.Fecha.ReadOnly = true;
+            this.Fecha.Width = 150;
+            // 
+            // btnRecibidos
+            // 
+            this.btnRecibidos.Location = new System.Drawing.Point(14, 12);
+            this.btnRecibidos.Name = "btnRecibidos";
+            this.btnRecibidos.Size = new System.Drawing.Size(75, 23);
+            this.btnRecibidos.TabIndex = 2;
+            this.btnRecibidos.Text = "Recibidos";
+            this.btnRecibidos.UseVisualStyleBackColor = true;
+            this.btnRecibidos.Click += new System.EventHandler(this.btnRecibidos_Click);
+            // 
+            // btnEnviados
+            // 
+            this.btnEnviados.Location = new System.Drawing.Point(104, 12);
+            this.btnEnviados.Name = "btnEnviados";
+            this.btnEnviados.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviados.TabIndex = 3;
+            this.btnEnviados.Text = "Enviados";
+            this.btnEnviados.UseVisualStyleBackColor = true;
+            this.btnEnviados.Click += new System.EventHandler(this.btnEnviados_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 358);
+            this.Controls.Add(this.btnEnviados);
+            this.Controls.Add(this.btnRecibidos);
+            this.Controls.Add(this.dataMails);
+            this.Name = "Form1";
+            this.Text = "Cliente mail";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataMails)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataMails;
+        private System.Windows.Forms.Button btnRecibidos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn De;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Asunto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
+        private System.Windows.Forms.Button btnEnviados;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Email.Net;
+using Email.Net.Common;
+using Email.Net.Pop3;
+using Email.Net.Common.Configurations;
+using Email.Net.Common.Collections;
+
+namespace clienteMail
+{
+    public partial class Form1 : Form
+    {
+        Pop3Client client;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnRecibidos_Click(object sender, EventArgs e)
+        {
+            this.dataMails.Rows.Clear();
+            Rfc822Message[] messages = client.GetAllMessages().ToArray();
+            
+            int i = 1;
+
+            foreach (Rfc822Message message in messages)
+            {
+                if(message.From.Address != client.Username){
+                    this.dataMails.Rows.Add(i.ToString(), message.From.DisplayName.ToString(), message.Subject.ToString(), message.Date.AddHours(-3).ToString());
+                    i++;
+                }
+                
+            }
+        }
+
+            void client_Connected(Pop3Client sender)
+            {
+                Console.WriteLine("Client connected");
+            }
+            void client_Authenticated(Pop3Client sender)
+            {
+                Console.WriteLine("Client authentificated");
+            }
+            void client_MessageReceived(Pop3Client sender, Rfc822Message message)
+            {
+                Console.WriteLine(string.Format("Message received: {0}", message.Subject));
+            }
+            void client_Completed(Pop3Client sender)
+            {
+                Console.WriteLine("Job completed");
+            }
+            void client_Quit(Pop3Client sender)
+            {
+                Console.WriteLine("Client left");
+            }
+            void client_BrokenMessage(Pop3Client sender, Pop3MessageInfo messageInfo, string errorMessage, Rfc822Message message)
+            {
+                Console.WriteLine(string.Format("message is broken: {0}", messageInfo.Number));
+            }
+            void client_MessageDeleted(Pop3Client sender, uint serialNumber)
+            {
+                Console.WriteLine(string.Format("message deleted: {0}", serialNumber));
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                // create client and connect 
+                client = new Pop3Client("pop.gmail.com", 995, "proyectofinal512@gmail.com", "proyecto123");
+
+                client.Connected += new Pop3ClientEventHandler(client_Connected);
+                client.Authenticated += new Pop3ClientEventHandler(client_Authenticated);
+                client.MessageReceived += new Pop3MessageEventHandler(client_MessageReceived);
+                client.Completed += new Pop3ClientEventHandler(client_Completed);
+                client.Quit += new Pop3ClientEventHandler(client_Quit);
+                client.BrokenMessage += new Pop3MessageInfoEventHandler(client_BrokenMessage);
+                client.MessageDeleted += new Pop3MessageIDHandler(client_MessageDeleted);
+
+                client.SSLInteractionType = EInteractionType.SSLPort;
+                // authenticate 
+                client.Login();
+                btnRecibidos_Click(null, EventArgs.Empty);
+
+            }
+
+            private void btnEnviados_Click(object sender, EventArgs e)
+            {
+                this.dataMails.Rows.Clear();
+
+                dataMails.Columns[1].HeaderText = "Para";
+                Rfc822MessageCollection messages = client.GetAllMessages();
+
+                int i = 1;
+                string para;
+                
+                foreach (Rfc822Message message in messages)
+                {
+                    if (message.From.Address == client.Username)
+                    {
+                        if (message.To.Count() > 1)
+                        {
+                            //hay mas de un destinatario
+                            para = message.To.Count().ToString() + " destinatarios";
+                        } else
+                        if(string.IsNullOrEmpty(message.To[0].DisplayName)){
+                            para = message.To[0].ToString();
+                        } else 
+                        {
+                            para = message.To[0].DisplayName;
+                        }
+                        
+                        this.dataMails.Rows.Add(i.ToString(), para, message.Subject.ToString(), message.Date.AddHours(-3).ToString());
+                        i++;
+                    }
+                    
+                }
+            }
+        
+    }
+}
