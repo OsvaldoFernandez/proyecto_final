033b8f867184107deeb2c9ef4976efd8407b1601
Federico do Pino <federico_dopino@fibertel.com.ar>
2015-11-13 10:20:50 -0300

    Eliminado login por default

--- clienteMail/inciar_sesion/iniciar_sesion.Designer.cs
+++ clienteMail/inciar_sesion/iniciar_sesion.Designer.cs
@@ -28,124 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(iniciar_sesion));
-            this.usertxt = new System.Windows.Forms.TextBox();
-            this.passtxt = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // usertxt
-            // 
-            this.usertxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.usertxt.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold);
-            this.usertxt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(135)))), ((int)(((byte)(138)))));
-            this.usertxt.Location = new System.Drawing.Point(17, 277);
-            this.usertxt.Name = "usertxt";
-            this.usertxt.Size = new System.Drawing.Size(384, 28);
-            this.usertxt.TabIndex = 0;
-            this.usertxt.Text = "proyectofinalgrupo512@gmail.com";
-            // 
-            // passtxt
-            // 
-            this.passtxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.passtxt.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold);
-            this.passtxt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(135)))), ((int)(((byte)(138)))));
-            this.passtxt.Location = new System.Drawing.Point(17, 352);
-            this.passtxt.Name = "passtxt";
-            this.passtxt.PasswordChar = '*';
-            this.passtxt.Size = new System.Drawing.Size(384, 28);
-            this.passtxt.TabIndex = 1;
-            this.passtxt.Text = "proyecto123";
-            // 
-            // button1
-            // 
-            this.button1.FlatAppearance.BorderSize = 3;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold);
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(117, 402);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(174, 50);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Iniciar sesión";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.75F, System.Drawing.FontStyle.Bold);
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(299, 488);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(102, 30);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Registrarse";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold);
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(112, 244);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(179, 30);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Cuenta de correo";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold);
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(139, 319);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(121, 30);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Contraseña";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(53, -88);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
-            // 
-            // iniciar_sesion
-            // 
-            this.AcceptButton = this.button1;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(183)))), ((int)(((byte)(248)))));
-            this.ClientSize = new System.Drawing.Size(413, 530);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.passtxt);
-            this.Controls.Add(this.usertxt);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.Name = "iniciar_sesion";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Speak Up! ";
-            this.Load += new System.EventHandler(this.iniciar_sesion_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(iniciar_sesion));
+      this.usertxt = new System.Windows.Forms.TextBox();
+      this.passtxt = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // usertxt
+      // 
+      this.usertxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+      this.usertxt.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold);
+      this.usertxt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(135)))), ((int)(((byte)(138)))));
+      this.usertxt.Location = new System.Drawing.Point(17, 277);
+      this.usertxt.Name = "usertxt";
+      this.usertxt.Size = new System.Drawing.Size(384, 28);
+      this.usertxt.TabIndex = 0;
+      // 
+      // passtxt
+      // 
+      this.passtxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+      this.passtxt.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold);
+      this.passtxt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(135)))), ((int)(((byte)(138)))));
+      this.passtxt.Location = new System.Drawing.Point(17, 352);
+      this.passtxt.Name = "passtxt";
+      this.passtxt.PasswordChar = '*';
+      this.passtxt.Size = new System.Drawing.Size(384, 28);
+      this.passtxt.TabIndex = 1;
+      // 
+      // button1
+      // 
+      this.button1.FlatAppearance.BorderSize = 3;
+      this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold);
+      this.button1.ForeColor = System.Drawing.Color.White;
+      this.button1.Location = new System.Drawing.Point(117, 402);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(174, 50);
+      this.button1.TabIndex = 2;
+      this.button1.Text = "Iniciar sesión";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.FlatAppearance.BorderSize = 0;
+      this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.75F, System.Drawing.FontStyle.Bold);
+      this.button2.ForeColor = System.Drawing.Color.White;
+      this.button2.Location = new System.Drawing.Point(299, 488);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(102, 30);
+      this.button2.TabIndex = 3;
+      this.button2.Text = "Registrarse";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold);
+      this.label1.ForeColor = System.Drawing.Color.White;
+      this.label1.Location = new System.Drawing.Point(112, 244);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(179, 30);
+      this.label1.TabIndex = 4;
+      this.label1.Text = "Cuenta de correo";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold);
+      this.label2.ForeColor = System.Drawing.Color.White;
+      this.label2.Location = new System.Drawing.Point(139, 319);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(121, 30);
+      this.label2.TabIndex = 5;
+      this.label2.Text = "Contraseña";
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+      this.pictureBox1.Location = new System.Drawing.Point(53, -88);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+      this.pictureBox1.TabIndex = 6;
+      this.pictureBox1.TabStop = false;
+      // 
+      // iniciar_sesion
+      // 
+      this.AcceptButton = this.button1;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(183)))), ((int)(((byte)(248)))));
+      this.ClientSize = new System.Drawing.Size(413, 530);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.passtxt);
+      this.Controls.Add(this.usertxt);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximizeBox = false;
+      this.Name = "iniciar_sesion";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Speak Up! ";
+      this.Load += new System.EventHandler(this.iniciar_sesion_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
         }
 
